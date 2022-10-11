d17f2fbbbab136c8cda444276f77148822e49c1c
Latasha02 <83638765+Latasha02@users.noreply.github.com>
2022-10-11 11:57:53 +0800

    Add files via upload
    First QB using queue

---
+++ FirstQB/src/MainClass.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        InputFrame myInpFrame = new InputFrame();
+        myInpFrame.setVisible(true);
+    }
+}
